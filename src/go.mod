499993d9229a90b3a21aa275c8afed7d55d6de20
josé <78055979+jossse69@users.noreply.github.com>
2024-03-19 19:40:33 -0300

    added src files

---
+++ src/go.mod
@@ -0,0 +1,12 @@
+module puzz
+
+go 1.22.1
+
+require (
+	github.com/ebitengine/purego v0.6.1 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20240227114648-c3665eb9abf8 // indirect
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240307211618-a69d953ea142 // indirect
+	github.com/veandco/go-sdl2 v0.4.38 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
---
+++ src/renderer/renderer.go
@@ -0,0 +1,57 @@
+package renderer
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type Renderer struct {
+	Target *rl.RenderTexture2D
+}
+
+const (
+	screenWidth  = 800
+	screenHeight = 600
+)
+
+// NewRenderer creates a new renderer for the game.
+//
+// It takes the game width, game height, and title as parameters and returns a pointer to Renderer.
+func NewRenderer(gameWidth, gameHeight int, title string) *Renderer {
+
+	rl.InitWindow(screenWidth, screenHeight, title)
+	rl.SetTargetFPS(60)
+
+	target := rl.LoadRenderTexture(int32(gameWidth), int32(gameHeight))
+	rl.SetTextureFilter(target.Texture, rl.FilterPoint)
+
+	return &Renderer{
+		Target: &target,
+	}
+}
+
+// DrawOnScreen draws the renderer on the screen.
+//
+// No parameters.
+// No return types.
+func (r *Renderer) DrawOnScreen() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Black)
+	rl.BeginTextureMode(*r.Target)
+	rl.ClearBackground(rl.Black)
+	gameWidth := float32(r.Target.Texture.Width)
+	gameHeight := float32(r.Target.Texture.Height)
+	rl.DrawTexturePro(r.Target.Texture,
+		rl.NewRectangle(0, 0, gameWidth, -gameHeight),
+		rl.NewRectangle(0, 0, float32(screenWidth), float32(screenHeight)),
+		rl.NewVector2(0, 0), 0, rl.White)
+	rl.EndDrawing()
+}
+
+// Close releases the render texture and closes the window.
+//
+// No parameters.
+// No return types.
+func (r *Renderer) Close() {
+	rl.UnloadRenderTexture(*r.Target)
+	rl.CloseWindow()
+}
